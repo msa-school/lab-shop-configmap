26a0c0aeb948fe19950714cb93537192cbcd2bd0
Apexacme <apexacme@example.com>
2023-05-13 12:44:44 +0900

    Order Service added.

---
+++ order/src/main/java/mall/OrderApplication.java
@@ -0,0 +1,20 @@
+package mall;
+
+import mall.config.kafka.KafkaProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderApplication {
+
+    public static ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        applicationContext =
+            SpringApplication.run(OrderApplication.class, args);
+    }
+}
---
+++ order/src/main/java/mall/infra/PolicyHandler.java
@@ -0,0 +1,22 @@
+package mall.infra;
+
+import javax.transaction.Transactional;
+import mall.config.kafka.KafkaProcessor;
+import mall.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class PolicyHandler {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    // @StreamListener(KafkaProcessor.INPUT)
+    // public void whatever(@Payload String eventString) {}
+    // // keep
+
+}
